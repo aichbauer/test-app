304189a172ad5bb4d545a8f4ac77960ba33c4bcd
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-04-19 12:55:44 +0200

    :bug: Fix: delete route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users_controller#index'
-  map.connect ":controller/:action/:id"
 end
